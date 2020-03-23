27ce494138e016b52eb6ca38f75233a099d03757
oovg <office@vengist.com>
2020-03-23 09:50:59 -0700

    starts Bg styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React, { useContext } from 'react';
-import { Container, Row, Col } from 'react-bootstrap'
-import { Web3SignIn } from './components/account/Web3SignIn';
-import { CurrentUserContext } from './contexts/Store';
-import { DepositForm } from './components/DepositForm';
-import { WithdrawForm } from './components/WithdrawForm';
-import { TokenInfo } from './components/TokenInfo';
-
+import React, { useContext } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { Web3SignIn } from "./components/account/Web3SignIn";
+import { CurrentUserContext } from "./contexts/Store";
+import { DepositForm } from "./components/DepositForm";
+import { WithdrawForm } from "./components/WithdrawForm";
+import { TokenInfo } from "./components/TokenInfo";
 
 function App() {
   const [currentUser, setCurrentUser] = useContext(CurrentUserContext);
@@ -14,15 +13,44 @@ function App() {
     <div className="App">
       <Container>
         <Row>
-          <Col>Weth Wrapper</Col>
           <Col>
-            {currentUser && currentUser.username ? (<p>{currentUser.username}</p>) : (<Web3SignIn setCurrentUser={setCurrentUser} />)}
+            <p>zUNIPoolz</p>
+          </Col>
+          <Col>
+            {currentUser && currentUser.username ? (
+              <p>{currentUser.username}</p>
+            ) : (
+              <Web3SignIn setCurrentUser={setCurrentUser} />
+            )}
           </Col>
         </Row>
+        <Row className="Hero">
+          <Col>
+            <h2>
+              ZIP into a Uniswap Pool to earn extra rewards for your liquidity.
+            </h2>
+            <a href="/about">Learn More</a>
+          </Col>
+          <Col></Col>
+        </Row>
         <Row>
-          <Col>{currentUser && currentUser.username ? (<DepositForm />) : (<p>Must sign in first</p>)}</Col>
-          <Col><TokenInfo /></Col>
-          <Col>{currentUser && currentUser.username ? (<WithdrawForm />) : (<p>Must sign in first</p>)}</Col>
+          <Col>
+            {currentUser && currentUser.username ? (
+              <DepositForm />
+            ) : (
+              <p>Must sign in first</p>
+            )}
+          </Col>
+          <Col>
+            <TokenInfo />
+          </Col>
+          <Col>
+            {currentUser && currentUser.username ? (
+              <WithdrawForm />
+            ) : (
+              <p>Must sign in first</p>
+            )}
+          </Col>
         </Row>
       </Container>
     </div>
